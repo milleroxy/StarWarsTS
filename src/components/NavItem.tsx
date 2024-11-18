a9ae18d03e656e0aab9acc35f24dcbfb91ae5419
luv2s <ox_m@list.ru>
2024-11-18 20:03:56 +0200

    First

---
+++ src/components/NavItem.tsx
@@ -0,0 +1,18 @@
+import {useContext} from "react";
+import {SWContext} from "../utils/context.ts";
+
+interface Props {
+    itemTitle: string,
+}
+
+const NavItem = ({itemTitle}: Props) => {
+
+
+    return (
+        <li className={`bg-red-color border-black border-2 rounded-[5px] px-3 py-2 cursor-pointer hover:text-white hover:bg-red-500`}>
+            <a href={`/${itemTitle}`}> {itemTitle} </a>
+        </li>
+    );
+};
+
+export default NavItem;
